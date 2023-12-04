8d409bdd042bf2f31c48b5edf207ac077dae7add
Wojciech Galazka <wojciech.galazka@gmail.com>
2023-12-04 16:46:43 +0100

    updated commit number

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -7,7 +7,7 @@
 ##############################################################
 
 #DONE: Fill up the contents below in order to reference your assignment 3 git contents
-LDD_VERSION = 919890fb9db2f7c8e376c27fc796b5995e15d3a7
+LDD_VERSION = 734901997bb9d00ecfa5d6854fb573c449d8f8b7
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
