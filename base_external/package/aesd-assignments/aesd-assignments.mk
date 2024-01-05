a168db5d93fa3aa15e55bdf55ed4d9f2540e8c74
Wojciech Galazka <wojciech.galazka@gmail.com>
2024-01-05 23:00:35 +0100

    configuration update for assignment8

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,4 +1,3 @@
-
 ##############################################################
 #
 # AESD-ASSIGNMENTS
@@ -6,7 +5,7 @@
 ##############################################################
 
 #DONE: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = c4483b11f4f649dd784aa60870dc5f41227d8ceb
+AESD_ASSIGNMENTS_VERSION = 83a2218509a12656384bf2d1b7fd47fdec72ddb3
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,18 +13,63 @@ AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-gala
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver 
+AESD_ASSIGNMENTS_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-#	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-#	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-#	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh      $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket               $(TARGET_DIR)/usr/bin/
-#	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 endef
 
+# see https://buildroot.org/downloads/manual/manual.html#_infrastructure_for_packages_building_kernel_modules (18.20)
+
+$(eval $(kernel-module))
+
+define AESD_ASSIGNMENTS_INSTALL_TARGET_MODULES
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load           $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload         $(TARGET_DIR)/usr/bin/
+endef
+
+AESD_ASSIGNMENTS_POST_BUILD_HOOKS += AESD_ASSIGNMENTS_INSTALL_TARGET_MODULES
+
 $(eval $(generic-package))
+ 
+#
+#	##############################################################
+#	#
+#	# LDD
+#	#
+#	# see https://buildroot.org/downloads/manual/manual.html#_infrastructure_for_packages_building_kernel_modules (18.20)
+#	#
+#	##############################################################
+#	
+#	#DONE: Fill up the contents below in order to reference your assignment 3 git contents
+#	LDD_VERSION = 734901997bb9d00ecfa5d6854fb573c449d8f8b7
+#	# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+#	# with ssh keys and the automated build/test system.
+#	# Your site should start with git@github.com:
+#	LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-galazwoj.git
+#	LDD_SITE_METHOD = git
+#	LDD_GIT_SUBMODULES = YES
+#	LDD_MODULE_SUBDIRS = misc-modules
+#	LDD_MODULE_SUBDIRS += scull
+#	LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+#
+#	$(eval $(kernel-module))
+#	
+#	define LDD_INSTALL_TARGET_MODULES
+#		$(INSTALL) -m 0755 $(@D)/scull/scull_load           $(TARGET_DIR)/usr/bin/
+#		$(INSTALL) -m 0755 $(@D)/scull/scull_unload         $(TARGET_DIR)/usr/bin/
+#		$(INSTALL) -m 0755 $(@D)/misc-modules/module_load   $(TARGET_DIR)/usr/bin/
+#		$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin/
+#	endef
+#
+#	LDD_POST_BUILD_HOOKS += LDD_INSTALL_TARGET_MODULES
+#	
+#	$(eval $(generic-package))
+#
+       
